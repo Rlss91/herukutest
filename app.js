01914537b6365bb3bd93df4c1e59839f5ddaca31
Rlss <rlss91@gmail.com>
2019-07-04 19:46:31 +0300

    first

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', (req, res)=>{
+    res.send("first haruku project")
+})
+
+app.listen(PORT)
